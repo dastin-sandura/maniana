4ef81577960e5de9134522eb06c4e202f4726f7c
DKuwalek <d.kuwalek@gmail.com>
2019-09-30 18:53:38 +0200

    Handle removing tasks from list
     - for every newly created task register a listener for its button which will remove it fro mthe list

--- app/models/task.js
+++ app/models/task.js
@@ -1,4 +1,5 @@
 export class Task {
+
     constructor(summary) {
         this.summary = "Task: " + summary;
     }
@@ -8,8 +9,22 @@ export class Task {
     }
 
     toParagraph() {
-        var $paragraphWithTaskSummary = document.createElement("p");
+        let $paragraphWithTaskSummary = document.createElement("p");
         $paragraphWithTaskSummary.innerText = this.summary;
+        let $doneButton = document.createElement("button");
+        $doneButton.innerText = "Task done!";
+        $doneButton.classList.add("task-done-button");
+        $doneButton.addEventListener('click', this.markTaskAsDone);
+        // console.log($doneButton.parentNode.parentNode.removeChild);
+        $paragraphWithTaskSummary.appendChild($doneButton);
         return $paragraphWithTaskSummary;
+    }
+
+    markTaskAsDone() {
+        console.log(this);
+        let parentParagraph = this.parentNode;
+        parentParagraph.parentNode.removeChild(parentParagraph);
+        // console.log(this.parentNode.removeChild(this));
+
     }
 }
