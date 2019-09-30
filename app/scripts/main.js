8292d6f4065b3dc5dd480b75fed48587feaecf11
DKuwalek <d.kuwalek@gmail.com>
2019-09-30 15:49:31 +0200

    Create form instead of one input #2
     - add the form element
     - add and change id of element to locate them easier in the code
     - function for reading value from any input by specified query selector
     - visible red text when form is not properly filled by user

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 import { Task } from '../models/task.js';
 
-let $taskList = document.querySelector('.task-list');
+let $registeredTasksList = document.querySelector('#registered-tasks-list');
 
 //End point from Mockapi.io website
 let mockDataEndpoint = 'http://5d8de5c5370f02001405c5e5.mockapi.io/maniana/mock-task-list';
@@ -14,29 +14,30 @@ fetch(mockDataEndpoint)
         let tasksData = body[0].tasks;
         tasksData.forEach(taskSummary => {
             let task = new Task(taskSummary);
-            $taskList.appendChild(task.toParagraph());
+            $registeredTasksList.appendChild(task.toParagraph());
         });
     });
 
-let $addNewItemButton = document.querySelector('#add-button');
+let getValueFromInput = function getValueFromInput(querySelector) {
+    let $inputElement = document.querySelector(querySelector);
+    // console.log($inputElement);
+    if (!$inputElement) {
+        throw new Error("Element was not found using the query selector " + querySelector)
+    }
+    return $inputElement.value;
+}
 
-let addNewTaskFunction = function () {
-    let taskSummary;
-    //todo replace logic using one input to logic which uses the HTML for element
-    let $newTaskForm = document.querySelector("#new-task-form");
-    let $summaryInput = document.querySelector("#task-input");
-    let summaryInputText = $summaryInput.value;
+let addNewTaskFunction = function addNewTaskFunction() {
+    let $newTaskSummaryInputInfo = document.querySelector("#new-task-summary-error");
+    let summaryInputText = getValueFromInput("#new-task-summary");
     if (summaryInputText) {
-        $newTaskForm.setAttribute("class", "");
-        taskSummary = summaryInputText;
-        let $taskItem = document.createElement("p");
-        $taskItem.innerText = taskSummary;
-        $taskList.appendChild($taskItem);
+        let newTask = new Task(summaryInputText);
+        $registeredTasksList.appendChild(newTask.toParagraph());
+        $newTaskSummaryInputInfo.setAttribute("class", "");
     } else {
-        $newTaskForm.setAttribute("class", "red");
-        //todo show error message visible to user in the span next to the input
-
+        $newTaskSummaryInputInfo.setAttribute("class", "red");
+        $newTaskSummaryInputInfo.innerText = "Summary cannot be empty";
     }
 };
-
+let $addNewItemButton = document.querySelector('#add-button');
 $addNewItemButton.addEventListener('click', addNewTaskFunction);
