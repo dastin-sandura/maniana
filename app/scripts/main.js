29c67b474b41710b4b5eda871b6c1d1d8b74c267
DKuwalek <d.kuwalek@gmail.com>
2019-09-30 18:12:22 +0200

    Keeping track of the most important goal
     - function which fills the most important task placeholder if any new task is being added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,8 +14,7 @@ fetch(mockDataEndpoint)
     .then(body => {
         let tasksData = body[0].tasks;
         tasksData.forEach(taskSummary => {
-            let task = new Task(taskSummary);
-            $registeredTasksList.appendChild(task.toParagraph());
+            addNewTaskWithGivenName(taskSummary);
         });
     });
 
@@ -28,17 +27,33 @@ let getValueFromInput = function getValueFromInput(querySelector) {
     return $inputElement.value;
 }
 
+let addNewTaskWithGivenName = function addNewTaskWithGivenName(taskName) {
+    let task = new Task(taskName);
+    $registeredTasksList.appendChild(task.toParagraph());
+    correctTheMostImportantTask(taskName);
+}
+
 let addNewTaskFunction = function addNewTaskFunction() {
     let $newTaskSummaryInputError = document.querySelector("#new-task-summary-error");
     let summaryInputText = getValueFromInput("#new-task-summary");
     if (summaryInputText) {
-        let newTask = new Task(summaryInputText);
-        $registeredTasksList.appendChild(newTask.toParagraph());
+        addNewTaskWithGivenName(summaryInputText);
         $newTaskSummaryInputError.setAttribute("class", "");
+        correctTheMostImportantTask(summaryInputText);
     } else {
         $newTaskSummaryInputError.setAttribute("class", "red");
         $newTaskSummaryInputError.innerText = "Summary cannot be empty";
     }
 };
+
+function correctTheMostImportantTask(summaryInputText) {
+    let $mostImportantTask = document.querySelector("#most-important-task p")
+
+    if (!$mostImportantTask) {
+        let $mostImportantTaskList = document.querySelector("#most-important-task")
+        let $newMostImportantTask = new Task(summaryInputText).toParagraph();
+        $mostImportantTaskList.appendChild($newMostImportantTask);
+    }
+}
 let $addNewItemButton = document.querySelector('#add-button');
 $addNewItemButton.addEventListener('click', addNewTaskFunction);
